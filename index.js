00df82e31eaa243256e4813aa487b725d1e4903d
suresh bethanasamy <suresh.b@WSEINML-GSRZFVH.local>
2019-11-22 16:35:25 +0530

    Updated the routes with patch and delete call

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Initialize the package
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,18 +10,17 @@ require('dotenv').config();
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use('/api', postRoutes);
-
 
 // Default routes
 app.get('/', (req,res) => {
     res.send('We are on home page');
 });
 
+// Mongoose connectivity
 mongoose.connect(process.env.DB_BASE_URL, {useNewUrlParser: true,useUnifiedTopology: true},() => {
     console.log('connected to DB');
 })
 
-// Listen the app
+// Listen the app with port
 app.listen(port, () => (console.log(`server running on ${port}`)));
--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,6 @@
 const routes = require('express').Router();
 const Post = require('../models/Post');
 
-
 // Get back all the post
 routes.get('/', async (req, res) => {
     try{
@@ -14,11 +13,12 @@ routes.get('/', async (req, res) => {
 
 // Submit the post
 routes.post('/', async (req, res) => {
- 
+ // Destructuring the data ( unpack the data with const variable )
     const { title, description } = req.body;
     const post = new Post({
         title,description
     });
+    
     try{
         const savedPost = await post.save();
         res.json(savedPost);
@@ -28,13 +28,45 @@ routes.post('/', async (req, res) => {
     
 });
 
-// Get specific all the post
+// Get specific a post with uniqueId
 routes.get('/:postId', async (req, res) => {
-    
+
+    // Destructuring the data ( unpack the data with const variable )
     const { postId} = req.params;
+
     try{
         const specificPost = await Post.findById(postId);
         res.json(specificPost);
+    }catch(err){
+        res.json({message: err})
+    }
+});
+
+// Update a specific the post with patch routes using uniqueId
+routes.patch('/:postId', async (req, res) => {
+
+    const { postId} = req.params;
+    const { title } = req.body;
+
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: postId},
+            { $set: { title }}
+        );
+        res.json(updatedPost);
+    }catch(err){
+        res.json({message: err})
+    }
+});
+
+// Remove a specific post with uniqueId
+routes.delete('/:postId', async (req, res) => {
+    
+    const { postId} = req.params;
+
+    try{
+        const deletedPost = await Post.deleteOne({_id: postId});
+        res.json(deletedPost);
     }catch(err){
         res.json({message: err})
     }
